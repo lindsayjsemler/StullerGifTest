62f3bfb59359887c1da23a8cd3be1a0fa6b47849
lindsayjsemler <lindsaysemler@yahoo.com>
2024-07-21 17:27:15 -0500

    Initial Commit of Stuller Gif Animation Test
    This is the initial commit of the Stuller Gif Animation Test
    TODO: 
    1). Read Me File with what needs to be downloaded to run and explanation of design
    2.) General code cleanup
    3.) Addition of any comments
    4.) Test out creation of lottie files or webview to show Gif Animation in Detail View 
    (Xamarin Forms cannot animate gifs in Android, need to update Xamarin Forms or change to Maui, which I cannot do due to limitations of my version of Mac iOS Big Sur)

---
+++ StullerGitTest/StullerGifTest/StullerGifTest/StullerGifTest/Views/DetailGifModal.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using StullerGifTest.Models;
+using StullerGifTest.ViewModels;
+using Xamarin.Forms;
+
+namespace StullerGifTest.Views
+{	
+	public partial class DetailGifModal : ContentPage
+    {
+        public GIFModel gifModel { get; set; }
+        public DetailGifModal(GIFModel _gifModel)
+        {
+            gifModel = _gifModel;
+            InitializeComponent();
+
+            //Set the binding context when navigated to and make sure to give the string to the viewmodel
+            BindingContext = new DetailGifViewModel(gifModel);
+        }
+
+        /*protected override void OnAppearing()
+        {
+            //On Appearing Start the Animation
+            GifImage.IsAnimationPlaying = true;
+
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            //stop this animation when the view is closing
+            if (GifImage.IsAnimationPlaying)
+            {
+                GifImage.CancelAnimations();
+            }
+
+            base.OnDisappearing();
+        }*/
+    }
+}
+
